ecc7dfedd3d8a63ca05b88f8badab2bbe48f6429
push <seleenherzalla@gmail.com>
2024-10-15 13:35:58 +0300

    Log in Page

--- lib/Views/RegisterScreen.dart
+++ lib/Views/RegisterScreen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -32,36 +36,43 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor:Colors.grey,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Phone Number Or Email : ", style: TextStyle(fontSize: 20,color: Colors.indigo) ),
-            Container(
-                width: 500,
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter Your Phone Number Or Email',
-                  ),
-                )
-            ),
-            Text("Password : ", style: TextStyle(fontSize: 20,color: Colors.indigo) ),
-            Container(
-                width: 500,
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter Password',
-                  ),
-                )
-            ),
-  ],
-      ),
-            ));
+        appBar: AppBar(
+          backgroundColor:Colors.grey,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Phone Number Or Email : ", style: TextStyle(fontSize: 20,color: Colors.indigo) ),
+              Container(
+                  width: 500,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter Your Phone Number Or Email',
+                    ),
+                  )
+              ),
+              Text("Password : ", style: TextStyle(fontSize: 20,color: Colors.indigo) ),
+              Container(
+                  width: 500,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter Password',
+                    ),
+                  )
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: (){print('Register');},child: Text('Register'),), SizedBox(width:10,),
+                    ElevatedButton(onPressed: (){ Navigator.pushNamed(context,'Sign Up', ); print('Sign Up');},child: Text('Sign Up'),),
+                  ]
+              ),
+            ],
+          ),
+        ));
   }
 }
